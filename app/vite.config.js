b5eb9ba292c1919788490aa002e1c71d4791f193
Andrew Mayer <andrewmayer515@gmail.com>
2021-12-17 00:09:13 -0600

    move proxy config

--- app/vite.config.js
+++ app/vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
   server: {
     port: 8080,
     open: true,
-  },
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-      secure: false,
-      ws: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
     },
   },
 });
