1118075b169054c4d0e89465d4d855b9171131d5
Andrew Mayer <andrewmayer515@gmail.com>
2022-02-11 21:36:00 -0600

    finish player/number results

--- server/src/routes/boxscore/index.js
+++ server/src/routes/boxscore/index.js
@@ -26,6 +26,49 @@ const BOXSCORE_INDEX = {
   POINTS: 14,
 };
 
+/**
+ * Determine where in the table the highest passed in statistic is (points, rebounds, etc)
+ * includes formatting
+ * @param {Array} table
+ * @param {Number} stat
+ * @returns object containing player name and recorded statistic value
+ */
+const getIndexArrayOfTopStatistic = (table, stat) => {
+  table.splice(0, 1);
+  table.splice(table.length - 1, 1);
+
+  let targetIndex = [0];
+  table.forEach((row, index) => {
+    if (
+      Number(row[stat].split('-')[0]) >
+      Number(table[targetIndex[0]][stat].split('-')[0])
+    ) {
+      targetIndex = [index];
+    } else if (
+      Number(row[stat].split('-')[0]) ===
+        Number(table[targetIndex[0]][stat].split('-')[0]) &&
+      index !== 0
+    ) {
+      targetIndex.push(index);
+    }
+  });
+
+  const player = targetIndex.map(index => table[index][BOXSCORE_INDEX.PLAYER]);
+  const formattedPlayer = player.map(name => {
+    const result = name.split(',');
+
+    return `${result[1]} ${result[0]}`;
+  });
+  const [number] = targetIndex.map(index =>
+    Number(table[index][stat].split('-')[0])
+  );
+
+  return {
+    player: formattedPlayer,
+    number,
+  };
+};
+
 const totalScore = (marquetteTable, opponentTable) => {
   const marquettePoints =
     marquetteTable[marquetteTable.length - 1][BOXSCORE_INDEX.POINTS];
@@ -55,30 +98,68 @@ const marquetteTotalThrees = table => {
   return Number(threes[0]);
 };
 
+const marquetteTopScorer = table =>
+  getIndexArrayOfTopStatistic(table, BOXSCORE_INDEX.POINTS);
+
+const marquetteTopAssistMan = table =>
+  getIndexArrayOfTopStatistic(table, BOXSCORE_INDEX.ASSISTS);
+
+const marquetteTopRebounder = table =>
+  getIndexArrayOfTopStatistic(table, BOXSCORE_INDEX.REBOUNDS);
+
+const marquetteTopThreePointShooter = table =>
+  getIndexArrayOfTopStatistic(table, BOXSCORE_INDEX.THREE_POINTERS);
+
 const resultRunner = (marquetteTable, opponentTable) => [
   {
     question: 1,
     result: totalScore(marquetteTable, opponentTable),
+    text: 'Total Game Points',
   },
   {
     question: 2,
     result: pointsScored(marquetteTable),
+    text: 'MU Points',
   },
   {
     question: 3,
     result: pointsScored(opponentTable),
+    text: 'Opponent Points',
   },
   {
     question: 4,
     result: totalTurnovers(opponentTable),
+    text: 'TOs forced by MU',
   },
   {
     question: 5,
     result: totalTurnovers(marquetteTable),
+    text: 'TOs forced by Opponent',
   },
   {
     question: 6,
     result: marquetteTotalThrees(marquetteTable),
+    text: 'MU total made 3s',
+  },
+  {
+    question: 7,
+    result: marquetteTopScorer(marquetteTable),
+    text: 'MU top scorer',
+  },
+  {
+    question: 8,
+    result: marquetteTopAssistMan(marquetteTable),
+    text: 'MU top assist man',
+  },
+  {
+    question: 9,
+    result: marquetteTopRebounder(marquetteTable),
+    text: 'MU top rebounder',
+  },
+  {
+    question: 10,
+    result: marquetteTopThreePointShooter(marquetteTable),
+    text: 'MU top 3-point shooter',
   },
 ];
 
